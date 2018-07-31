a12ed9f68b50f56573bbaccf3d7869f5b209d946
Xiaoyu Zhang <xiaoyu.zhang@xzhang-mbp.corp.symphony.com>
2018-07-30 17:20:07 -0700

    update injestion date and duplicate them)

--- payload.js
+++ payload.js
@@ -14,25 +14,32 @@ function readJsonFileAsync(filepath, callback) {
   });
 }
 
-// generate multiple messages for different rooms
-readJsonFileAsync("./message.json", (err, message) => {
+let lastUpdate = new Date().getTime() - 3600 * 1000; // last hour
+// duplicate the message 50 times
+readJsonFileAsync("./message.json", (err, messages) => {
   const payload = {
     lastAccessTime: 0,
     ackId: "string",
     eventsDropped: true,
     events: []
   };
-  const messages = [];
-  for (let i = 0, max = 2; i < max; i++) {
-    messages.push({
-      feedId: "string",
-      eventId: "string",
-      payload: {
-        ...message,
-        messageId: `${message.messageId}-${i}`
-      }
-    });
+  const newMessages = [];
+  for (let i = 0, max = 60; i < max; i++) {
+    newMessages.push(
+      ...messages
+        .map(message => ({
+          feedId: "string",
+          eventId: "string",
+          payload: {
+            ...message,
+            messageId: `${message.messageId}-${i}`,
+            ingestionDate: lastUpdate + Math.random() * 1000
+          }
+        }))
+        .sort((a, b) => a.ingestionDate - b.ingestionDate)
+    );
+    lastUpdate += 1000; // next mintue
   }
-  payload.events = messages;
-  console.log(JSON.stringify(messages));
+  payload.events = newMessages;
+  console.log(JSON.stringify(newMessages));
 });
